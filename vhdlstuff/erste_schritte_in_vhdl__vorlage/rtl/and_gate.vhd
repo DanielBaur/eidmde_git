6eb7b5bcd0027ebff118a2288854f0bd5c142273
db1086 <db1086@cip18.physik.privat>
2021-07-28 17:31:20 +0200

    20210728_after_exercise_push

--- vhdlstuff/erste_schritte_in_vhdl__vorlage/rtl/and_gate.vhd
+++ vhdlstuff/erste_schritte_in_vhdl__vorlage/rtl/and_gate.vhd
@@ -13,11 +13,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- ----------------------------------------
 
 entity and_gate is
-    Port (
-
-        -- Define input and output signals here.
-
-    );
+    Port ( A : in STD_LOGIC;
+			  B : in STD_LOGIC;
+			  C : out STD_LOGIC);
 end and_gate;
 
 
@@ -30,7 +28,7 @@ architecture Behavioral of and_gate is
 
 begin
 
-    -- Assign signals here.
+    C <= A and B;
 
 end Behavioral;
 
